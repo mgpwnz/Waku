7a36614cc13fe7c0cad8d077fb9b5007a7ea28d3
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-06-25 18:01:54 +0300

    update

--- waku.sh
+++ waku.sh
@@ -63,7 +63,7 @@ git pull
 rm .env && cp .env.example .env
 if [ ! $RPC ]; then
 		read -p "Enter RPC : " RPC
-		echo 'export HTTPS='${RPC} >> $HOME/.bash_profile
+		echo 'export RPC='${RPC} >> $HOME/.bash_profile
 	fi
 if [ ! $EPK ]; then
 		read -p "Enter EVM private key : " EPK
