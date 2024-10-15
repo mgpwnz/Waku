7ed0564901bea2c2e6c7b691bc47711aaf423be1
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-10-15 21:39:52 +0300

    updt

--- waku.sh
+++ waku.sh
@@ -67,9 +67,11 @@ if [ ! -f $HOME/backup_nwaku/keystore.json ]; then
   cp $HOME/nwaku-compose/keystore/keystore.json $HOME/backup_nwaku/keystore.json
 fi
 #git pull https://github.com/waku-org/nwaku-compose.git
-git stash
-git pull
-git stash apply
+#git stash
+#git pull
+#git stash apply
+git pull origin master
+bash $HOME/nwaku-compose/set_storage_retention.sh
 rm .env && cp .env.example .env
 
 if [ ! $RPC ]; then
