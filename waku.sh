b2e412d21205f155eb1273363d2aed165fe31610
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-12-14 02:22:14 +0200

    changes

--- waku.sh
+++ waku.sh
@@ -5,7 +5,7 @@ do
 # Menu
 
 PS3='Select an action: '
-options=("Docker" "Download the components" "Create the configuration" "Check health" "Run Node" "Update Node" "Logs" "Uninstall" "Exit")
+options=("Docker" "Download the components" "Create the configuration" "Run Node" "Update Node" "Logs" "Check health" "Uninstall" "Exit")
 #options=("Docker" "Download the components" "Create the configuration" "Run Node" "Update Node" "Upgrade Node" "Logs" "Uninstall" "Exit")
 select opt in "${options[@]}"
                do
